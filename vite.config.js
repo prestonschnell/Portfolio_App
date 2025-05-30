2ddde02f8ea32f9740496f7deb6e7566614ee63c
Preston Schnell <64565042+prestonschnell@users.noreply.github.com>
2025-05-30 16:58:52 -0500

    Optimizing for responsiveness.

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Coding-Projects/',
+  base: '/Portfolio_App/',
   plugins: [react()],
 });
